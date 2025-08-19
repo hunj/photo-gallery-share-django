0432508d2e8f2de80b3ea3b93345ea911613c5ec
hunj <hunjaelee@protonmail.com>
2025-08-19 10:59:04 -0400

    fix agent mistakes

--- app/core/settings.py
+++ app/core/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "deez_nuts")
+SECRET_KEY = os.environ.get("SECRET_KEY", "deez_nuts")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split()
 
 
 # Application definition

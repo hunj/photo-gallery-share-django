d3d5e410a282d52e2975ab46ccef32f402af8196
hunj <hunjaelee@protonmail.com>
2025-08-24 11:23:59 -0400

    roiuting

--- app/core/urls.py
+++ app/core/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("time/", views.time_partial, name="time-partial"),
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("", include("gallery.urls")),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
